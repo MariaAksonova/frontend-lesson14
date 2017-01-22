e63e1e2f95a6a659a1e87fdc3d662d7a0eb84caf
Maria Aksonova <mashaaks@ukr.net>
2017-01-22 23:06:09 +0200

    SCSS

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $('.our-work .wrapper').masonry({
         itemSelector: '.grid-item',
         columnWidth: 260,
+        fitWidth: true,
         singleMode: false,
         isResizable: true,
         isAnimated: true,
